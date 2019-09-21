65e922cf6b579ae1591ef1f909e58a5896ee0887
berhalak <jaroslaw.sadzinski@gmail.com>
2019-09-21 23:12:02 +0200

    Memo redesign

--- src/Memo.ts
+++ src/Memo.ts
@@ -1,17 +1,17 @@
 export interface MemoPort {
-    save(ref: string, obj: any): Promise<void>;
-    read(ref: string): Promise<any>;
-    all(path: string): Promise<any[]>;
-    find(path: string, filter: string): Promise<any[]>;
+    save(ref: string, model: any): Promise<void>;
+    load(ref: string): Promise<any>;
+    id(): string;
 }
 
 export class InMemory implements MemoPort {
     public db: any = {}
+    private _id = 0;
     save(ref: string, obj: any): Promise<void> {
         this.db[ref] = obj;
         return Promise.resolve();
     }
-    read(ref: string): Promise<any> {
+    load(ref: string): Promise<any> {
         return Promise.resolve(this.db[ref]);
     }
     all(path: string): Promise<any[]> {
@@ -30,7 +30,16 @@ export class InMemory implements MemoPort {
     find(path: string, filter: string): Promise<any[]> {
         return this.all(path);
     }
+
+    id() {
+        this._id++;
+        return this._id.toString();
+    }
 }
+
+const s_parent = Symbol.for("$parent");
+const s_ref = Symbol.for("$ref");
+const s_id = Symbol.for("$id");
 
 export class Memo {
     private static port: MemoPort = new InMemory();
@@ -38,6 +47,9 @@ export class Memo {
         Memo.port = port;
     }
     public static id(any: any): string {
+        if (typeof any == 'string') {
+            return any;
+        }
         let id = null;
         if (!any) {
             throw new Error();
@@ -48,17 +60,33 @@ export class Memo {
         else if (any.id) {
             id = any.id;
         }
+        else if (any[s_id]) {
+            id = any[s_id];
+        }
+
+        if (!id) {
+            id = Memo.port.id();
+            Memo.setId(id, any);
+        }
+
         return id;
     }
+
     public static self(any: any): string {
         return Memo.parent(any) + "/" + Memo.type(any) + "/" + Memo.id(any);
-
     }
+
     public static ref(any: any): string {
         if (typeof any.ref == 'function') {
             return any.ref();
         }
-        return Memo.self(any);
+        if (any[s_ref]) {
+            return any[s_ref];
+        }
+        const ref = Memo.self(any);
+        Memo.setRef(ref, any);
+        return ref;
+
     }
     public static type(any: any): string {
         if (typeof any.type == 'function') {
@@ -82,16 +110,29 @@ export class Memo {
         let parts = type.split("#");
         return parts[0];
     }
+
     public static attach<T>(parent: any, child: T): T {
-        (child as any)['$parent'] = Memo.ref(parent);
+        if (child && typeof (child as any).parent == 'function') {
+            return child;
+        }
+        if ((child as any)[s_parent]) {
+            return child;
+        }
+        Object.defineProperty(child, s_parent, {
+            value: typeof parent == 'string' ? parent : Memo.ref(parent),
+            configurable: false,
+            enumerable: false,
+            writable: false
+        })
         return child;
     }
+
     public static parent(any: any): string {
         if (typeof any.parent == 'function') {
             return any.parent();
         }
-        if (any['$parent']) {
-            return any["$parent"];
+        if (any[s_parent]) {
+            return any[s_parent];
         }
         return "";
     }
@@ -104,12 +145,14 @@ export class Memo {
 
     public static current: Memo = new Memo();
 
-    public static save(obj: any): Promise<string> {
-        return Memo.current.save(obj);
+    public static snap(obj: any): Promise<string> {
+        return Memo.current.snap(obj);
     }
 
-    public static read(ref: string): Promise<any> {
-        return Memo.current.read(ref);
+    public static load(ref: string): Promise<any> {
+        let loaded = Memo.current.load(ref);
+
+        return loaded;
     }
 
     private parentRef: string | undefined;
@@ -129,33 +172,87 @@ export class Memo {
         }
     }
 
-    public async read(refId: string): Promise<any> {
+    static setRef(ref: string, obj: any) {
+        if (obj.ref && typeof obj.ref == 'function') {
+            return;
+        }
+        if ((obj as any)[s_ref]) {
+            return;
+        }
+        Object.defineProperty(obj, s_ref, {
+            value: ref,
+            configurable: false,
+            enumerable: false,
+            writable: false
+        })
+    }
+
+    static setId(ref: string, obj: any) {
+        if (obj.id && typeof obj.id == 'function') {
+            return;
+        }
+        if ((obj as any)[s_id]) {
+            return;
+        }
+        if (obj.id) {
+            return;
+        }
+        Object.defineProperty(obj, s_id, {
+            value: ref,
+            configurable: false,
+            enumerable: false,
+            writable: false
+        })
+    }
+
+    public static async look(parent: any, ctr: { name: string }, id: string): Promise<any> {
+        return null;
+    }
+
+    public async load(refId: string): Promise<any> {
+        let loaded = null;
+        let ref = '';
         if (this.parentRef !== undefined) {
             if (!refId.includes("/")) {
                 // it has only id
-                let proper = this.parentRef + "/" + (this.defaultType || "Object") + "/" + refId;
-                return await Memo.port.read(proper);
+                ref = this.parentRef + "/" + (this.defaultType || "Object") + "/" + refId;
+
+                loaded = await Memo.port.load(ref);
             } else {
-                let proper = this.parentRef + "/" + refId;
-                return await Memo.port.read(proper);
+                ref = this.parentRef + "/" + refId;
+                loaded = await Memo.port.load(ref);
             }
         } else {
             if (!refId.startsWith("/")) {
-                return await Memo.port.read("/Object/" + refId);
+                ref = "/Object/" + refId;
+                loaded = await Memo.port.load(ref);
+            } else {
+                ref = refId;
+                loaded = await Memo.port.load(ref);
             }
-            return await Memo.port.read(refId);
+        }
+        if (loaded) {
+
+            if (loaded.ref && typeof loaded.ref == 'function') {
+                return loaded;
+            }
+
+            Memo.setRef(ref, loaded);
+
+            return loaded;
         }
     }
 
-    public async save(obj: any): Promise<string> {
+    public async snap(obj: any): Promise<string> {
         if (this.parentRef !== undefined) {
             let type = this.defaultType || Memo.type(obj);
             let ref = this.parentRef + "/" + type + "/" + Memo.id(obj);
             await Memo.port.save(ref, obj);
             return ref;
         } else {
-            await Memo.port.save(Memo.ref(obj), obj);
-            return Memo.ref(obj);
+            let ref = Memo.ref(obj);
+            await Memo.port.save(ref, obj);
+            return ref;
         }
     }
 }
